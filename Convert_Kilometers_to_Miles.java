ea736a7ad51986f1c2694af2a64a41eadb249b54
Your Name <youremail@example.com>
2025-01-08 00:17:15 +0530

    This is Program to Convert Kilometers to Miles

---
+++ Convert_Kilometers_to_Miles.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Convert_Kilometers_to_Miles{
+	public static void main(String [] args){
+		double kilometers, Miles;
+		Scanner sc=new Scanner(System.in);
+		kilometers=sc.nextDouble();
+		Miles=kilometers* 0.621371;
+	System.out.println(Miles);
+	}
+}
